3e2d7b66a5969086a8db976a135abce20f6338e9
CEPHAL0 <xaradxarma@gmail.com>
2024-11-16 03:18:12 +0545

    Added Routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import * as React from 'react';
+import * as ReactDOM from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from './routes.tsx';
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
---
+++ src/routes.tsx
@@ -0,0 +1,7 @@
+import App from "./App"
+export const routes = [
+    {
+        path: "/",
+        element: <App/>
+    },
+]

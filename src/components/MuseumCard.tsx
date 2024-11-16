2c858a7fc3856c7c2b7daeca34ec6307911367a2
nishant-nez <nishant.khadka@deerwalk.edu.np>
2024-11-16 14:40:18 +0545

    1.1

--- src/components/MuseumCard.tsx
+++ src/components/MuseumCard.tsx
@@ -3,7 +3,7 @@ const MuseumCard = ({ museumImg, title, description, status }: { museumImg: any,
         <div className="flex flex-col gap-2 hover:scale-110 transition duration-500 cursor-pointer">
             <img
                 src={museumImg}
-                className="rounded-t-full h-[290px] w-[220px] "
+                className="rounded-t-full h-[290px] w-[220px] object-cover"
                 alt="" />
             <p className='text-2xl font-semibold'>{title}</p>
             <p className='font-light'>{description}</p>

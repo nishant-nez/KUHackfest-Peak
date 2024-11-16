2e9126aea5dd44e5b4921fd596f4a2795e9fe8c3
CEPHAL0 <xaradxarma@gmail.com>
2024-11-16 11:59:13 +0545

    Added Files

--- src/components/MuseumStoryCard.tsx
+++ src/components/MuseumStoryCard.tsx
@@ -1,19 +1,29 @@
 import museumstory from "../assets/images/museumstory.jpg";
 
-export default function MuseumStoryCard() {
+type MuseumStoryCardProps = {
+    name: string;
+    image: string;
+    title: string;
+    date: string;
+    action: string;
+    description: string;
+}
+
+export default function MuseumStoryCard({ name, image, title, date, action, description }: MuseumStoryCardProps) {
     return <div className="">
-        <div className="flex gap-2 items-center">
-            <p>London Museum Docklands </p>
+
+        <div className="flex gap-2 items-center mb-7">
+            <p>{name} </p>
             <i className="fa-solid fa-location-dot"></i>
         </div>
-        <div className="relative w-96 flex flex-col gap-2">
-            <img src={museumstory} alt="Museum Story" className="w-full object-cover" />
+        <div className="relative w-96 flex flex-col gap-1 text-zinc-600">
+            <img src={image} alt="Museum Story" className="w-full object-cover" />
             <p className="bg-white p-2 text-sm absolute top-4 left-4">Free</p>
 
-            <p className="w-full word-wrap">The Story of Rememberance of Story UK</p>
-            <p className="text-gray-600">14 Dec 2024</p>
-            <p>Talk</p>
-            <p>Marking 30 Years of American Independence Day</p>
+            <p className="w-full  text-black word-wrap text-xl mt-4">{title}</p>
+            <p className="text-gray-600">{date}</p>
+            <p>{action}</p>
+            <p className="w-[60%] word-wrap">{description}</p>
         </div>
     </div>
 }
--- src/components/MuseumStoryCards.tsx
+++ src/components/MuseumStoryCards.tsx
@@ -1,10 +1,31 @@
 import MuseumStoryCard from "./MuseumStoryCard";
+import './css/styles.css';
+
+import museumstory1 from "../assets/images/museumstory2.jpg";
+import museumstory2 from "../assets/images/museumstory.jpg";
+import museumstory3 from "../assets/images/museumstory3.jpg";
 
 export default function MuseumStoryCards() {
-    return <div className="">
-        <p className="text-3xl pl-10 uppercase mb-5">
-            What's on
-        </p>
-        <MuseumStoryCard />
+    return <div className="relative">
+        <p className="text-3xl pl-10 uppercase mb-5"> What's on </p>
+
+        <div className="overflow-x-scroll flex gap-4 hide-scrollbar px-10 py-6">
+            <div className="absolute right-10 top-5 z-50">
+                <button className="bg-black py-5 px-3 text-white hover:outline-black hover:bg-white hover:text-black transition-colors ease-in-out duration-100 hover:outline">
+                    <i className="fa-solid fa-arrow-left"></i>
+                </button>
+                <button className="bg-black py-5 px-3 text-white hover:outline-black hover:bg-white hover:text-black transition-colors ease-in-out duration-100 hover:outline">
+                    <i className="fa-solid fa-arrow-right"></i>
+                </button>
+            </div>
+            <MuseumStoryCard
+                name="Patan"
+                image={museumstory1}
+                title="Hello"
+                date="2024 Dec 14"
+                action="Talk"
+                description="This is a museum story with high content"
+            />
+        </div>
     </div>
 }
